d8bd3f1479e8236c19f70ceff7d6fd272465ac3b
Aaron P. Rykhus <aprykhus@hotmail.com>
2019-07-12 14:38:56 -0500

    Add min/max SongIDs for First/Last buttons

--- song.php
+++ song.php
@@ -1,5 +1,6 @@
 <?php
-$id = $_REQUEST["id"];
+$id = $_REQUEST["id"]; # songID to query in db
+$qtype = $_REQUEST["qtype"]; # Type of query 1 = song row 2 = min/max SongID
 /* Specify the server and connection string attributes. */
 $serverName = "(local)\SQLEXPRESS";
 $connectionInfo = array( "Database"=>"Music");
@@ -12,32 +13,37 @@ if( $conn === false )
     die( print_r( sqlsrv_errors(), true));
 }
 
-/* Query Music db for song */
-$tsql = "SELECT * FROM vw_ListSongs WHERE SongID = ".$id;
-$stmt = sqlsrv_query($conn, $tsql);
-if( $stmt === false)
+if ($qtype == 1)
 {
-    echo "Error in executing query.</br>";
-    dir( print_r( sqlsrv_errors(), true));
+    /* Query Music db for song */
+    $tsql = "SELECT * FROM vw_ListSongs WHERE SongID = ".$id;
+    $stmt = sqlsrv_query($conn, $tsql);
+    if( $stmt === false)
+    {
+        echo "Error in executing query.</br>";
+        die( print_r( sqlsrv_errors(), true));
+    }
+
+    /* Retreive and display the results of the query. */
+    $row = sqlsrv_fetch_array($stmt);
+    $space = " ";
+    echo json_encode($row);
 }
 
-/* Retreive and display the results of the query. */
-$row = sqlsrv_fetch_array($stmt);
-$space = " ";
-echo json_encode($row);
-
-/* Fetch min/max songIDs */
-
-/* $tsql = "Select MIN(SongID) AS minSongID, Select MAX(SongID) AS maxSongID FROM vw_ListSongs";
-$stmt = sqlsrv_query( $conn, $tsql);
-if( $stmt === false)
+if ($qtype == 2)
 {
-    echo "Error in executing query.</br>";
-    dir( print_r( sqlsrv_errors(), true));
-}
-$row = sqlsrv_fetch_array($stmt);
-echo json_encode($row); */
+    /* Fetch min/max songIDs */
 
+    $tsql = "Select MIN(SongID) AS minSongID, MAX(SongID) AS maxSongID FROM vw_ListSongs";
+    $stmt = sqlsrv_query( $conn, $tsql);
+    if( $stmt === false)
+    {
+        echo "Error in executing query.</br>";
+        die( print_r( sqlsrv_errors(), true));
+    }
+    $row = sqlsrv_fetch_array($stmt);
+    echo json_encode($row);
+}
 
 /* Free statement and connection resources. */
 sqlsrv_free_stmt( $stmt);
